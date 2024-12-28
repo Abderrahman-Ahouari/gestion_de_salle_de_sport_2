ea054b97633b526fb8eb91177b472c0e62a6dfc4
lakrouehamza <lakrounehamza10@gmail.com>
2024-12-28 11:13:40 +0100

    liste les resevation et activite de client

--- views/user_dashboard.php
+++ views/user_dashboard.php
@@ -32,8 +32,14 @@ while($row = $stmtR->fetch(PDO::FETCH_ASSOC)) {
             <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"><?php echo $row['statut']; ?></span>
         </td>
         <td class="px-6 py-4 whitespace-nowrap">
-            <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out" name="edit_<?php echo $row['nomPrenom']; ?>">Edit</button>
-            <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out" name="delete_<?php echo $row['nomPrenom']; ?>">Delete</button>
+        <form method="POST" > 
+                <input value="" name="RESERVATION" class="hidden" />   
+                <input class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out" name="edit" type="submit" value="Edit" />
+            </form>
+            <form method="POST" > 
+                <input value="" name="RESERVATION" class="hidden" />   
+                <input class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out" name="delete" type="submit" value="DELETE" />
+            </form>
         </td>
     </tr>
 <?php } ?>
